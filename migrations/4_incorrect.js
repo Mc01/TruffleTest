7d4c9d0e36f370c2194fd0ce254230df87665242
Marcin Majchrzak <marcin.majchrzak@10clouds.com>
2018-05-30 01:09:46 +0200

    Display comments.

--- migrations/4_incorrect.js
+++ migrations/4_incorrect.js
@@ -2,9 +2,11 @@ const IncorrectChild = artifacts.require("./IncorrectChild.sol");
 
 module.exports = (deployer) => {
     deployer.deploy(IncorrectChild).then(() => {
+        console.log('This should be the default output for Truffle');
         try { console.log(IncorrectChild.address); }
         catch(e) { console.log(e) }
 
+        console.log('This should throw during deploy and do not allow calling contract inside')
         console.log(IncorrectChild.address);
     });
 };
